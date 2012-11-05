ab212b96e23c323a0ae177d8700b36fa563381da
Mihai Bivol <mm.bivol@gmail.com>
2012-11-05 20:03:26 +0200

    Add draft models for existing feature request

--- rppl/people/models.py
+++ rppl/people/models.py
@@ -5,9 +5,9 @@ class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    username = models.CharField(max_length=64, default='', blank=True, help_text='Username de pe swarm')
-    nickname = models.CharField(max_length=64, default='', blank=True, help_text='IRC nickname')
     email = models.CharField(max_length=100)
+
+    description = models.CharField(max_length=200)
 
     class Meta:
         unique_together = ('first_name', 'last_name')
@@ -19,59 +19,33 @@ class Person(models.Model):
     def __unicode__(self):
         return self.name
 
-class Activity(models.Model):
-    STATUSES = ((0, 'Active'), (1, 'Suspended'), (2, 'Defunct'))
+class Link(models.Model):
+    """ Link for person's external accounts """
+    url = models.CharField(max_length=100)
+    person = models.ForeignKey(Person)
 
+class Organization(models.Model):
+    """ External affiliations for users """
+    url = models.CharField(max_length=100)
+    persons = models.ManyToManyField(Person)
+
+class Project(models.Model):
+    """ Project in community """
     name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=64, blank=True, default='')
-    status = models.IntegerField(choices=STATUSES, default=0)
-    description = models.TextField(max_length=2000, help_text='Markdown text')
+    url = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
     logo = models.ImageField(blank=True, null=True, upload_to=settings.MEDIA_ROOT)
 
-    def is_active(self):
-        return self.status == 0
-
-    @property
-    def editions(self):
-        return self.edition_set.order_by('-date_start')
-
-    def __unicode__(self):
-        return self.name
-
-class Event(Activity):
-    """ Public event
-    """
-    location = models.CharField(max_length=100, blank=True, null=True)
-
-class Project(Activity):
-    """ Software project.
-    """
-    repo = models.CharField(max_length=100, blank=True, null=True)
-
-class Version(models.Model):
-    project = models.ForeignKey(Activity)
-    repo = models.CharField(max_length=100, blank=True, default='')
-    version = models.CharField(max_length=100, help_text='Numele versiunii')
-
-    managers = models.ManyToManyField(Person, related_name='projectmanaged', null=True, blank=True)
-    developers = models.ManyToManyField(Person, related_name='developed', null=True, blank=True)
-
-    def __unicode__(self):
-        return self.version
-
 class Edition(models.Model):
-    event = models.ForeignKey(Activity)
-    title = models.CharField(max_length=100, help_text='Titlu, gen: CDL 2012 primavara')
-    description = models.TextField(max_length=2000, help_text='Markdown text, overrides Activity', null=True, blank=True)
-    logo = models.ImageField(blank=True, null=True, upload_to=settings.MEDIA_ROOT, help_text='Logo, overrides Activity')
-    date_start = models.DateField()
-    date_end = models.DateField(null=True, blank=True)
+    """ Project edition """
+    project = models.ForeignKey(Project)
+    picture = models.ImageField(blank=True, null=True, upload_to=settings.MEDIA_ROOT)
+    name = models.CharField(max_length=100)
 
-    managers = models.ManyToManyField(Person, related_name='managed', null=True, blank=True)
-    helpers = models.ManyToManyField(Person, related_name='helped', null=True, blank=True)
+class Role(models.Model):
+    """ A role that can be given to many persons in an editition """
+    name = models.CharField(max_length=100)
 
-    def get_description(self):
-        return self.description or self.event.description
-
-    def __unicode__(self):
-        return self.title
+    edition = models.ForeignKey(Edition)
+    persons = models.ManyToManyField(Person)
