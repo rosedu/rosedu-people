2f12b933ba7b9752574b8009142d199be99b0531
Mihai Bivol <mm.bivol@gmail.com>
2012-11-06 01:18:14 +0200

    Add draft pages for projects

--- rppl/people/urls.py
+++ rppl/people/urls.py
@@ -5,6 +5,6 @@ from views import Overview, Profile, Projects, ProjectDetail
 urlpatterns = patterns('',
     url(r'^$', Overview.as_view(), name='overview'),
     url(r'^profile/(?P<pk>\d+)/$', Profile.as_view(), name='profile'),
-    url(r'^activity/$', Projects.as_view(), name='activity-list'),
-    url(r'^activity/(?P<pk>\d+)/$', ProjectDetail.as_view(), name='activity-detail'),
+    url(r'^project/$', Projects.as_view(), name='project-list'),
+    url(r'^project/(?P<pk>\d+)/$', ProjectDetail.as_view(), name='project-detail'),
 )
