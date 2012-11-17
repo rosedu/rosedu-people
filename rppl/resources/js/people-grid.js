4465ba8c7775ad2f0422bd5401acf2a0dd78d007
piatra <andrei.br92@gmail.com>
2012-11-17 12:45:28 +0200

    grid image zoom feature

--- rppl/resources/js/people-grid.js
+++ rppl/resources/js/people-grid.js
@@ -1,11 +1,9 @@
-$('.person').hover(
-        function()
-        {
-            $(this).find('.avatar-container-big').css({opacity: 0.0, visibility: 'visible'}).animate({opacity: 1.0, width: '180px', height: '180px'}, 500);
-        },
-        function()
-        {
-            $(this).find('.avatar-container-big').css({opacity: 0.0, visibility: 'hidden'}).animate({opacity: 0, width: '60px', height: '60px'}, 100);
-        });
-
-
+// $('.person').hover(
+//         function()
+//         {
+//             $(this).find('.avatar-container-big').css({opacity: 0.0, visibility: 'visible'}).animate({opacity: 1.0, width: '180px', height: '180px'}, 500);
+//         },
+//         function()
+//         {
+//             $(this).find('.avatar-container-big').css({opacity: 0.0, visibility: 'hidden'}).animate({opacity: 0, width: '60px', height: '60px'}, 100);
+//         });
