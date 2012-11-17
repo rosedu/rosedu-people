7d4a883d3198ba7989d3e5d40c2099d641b2034b
piatra <andrei.br92@gmail.com>
2012-11-17 12:48:29 +0200

    deleted grid.js

--- rppl/resources/js/people-grid.js
+++
@@ -1,9 +0,0 @@
-// $('.person').hover(
-//         function()
-//         {
-//             $(this).find('.avatar-container-big').css({opacity: 0.0, visibility: 'visible'}).animate({opacity: 1.0, width: '180px', height: '180px'}, 500);
-//         },
-//         function()
-//         {
-//             $(this).find('.avatar-container-big').css({opacity: 0.0, visibility: 'hidden'}).animate({opacity: 0, width: '60px', height: '60px'}, 100);
-//         });
