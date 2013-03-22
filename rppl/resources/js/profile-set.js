b67398d6ae8c4539e20da46d00d75ae662d036a6
Mihai Bivol <mm.bivol@gmail.com>
2013-03-23 01:07:39 +0200

    Allow user to add new links.

---
+++ rppl/resources/js/profile-set.js
@@ -0,0 +1,35 @@
+
+var removableInputs = document.getElementsByClassName('removable-field');
+var nextLink = document.getElementsByClassName('link-field').length;
+var noLinks = nextLink;
+
+document.querySelector('#add-link').addEventListener('click',
+        function() {
+            var linkDiv = document.createElement('div');
+            linkDiv.classList.add('removable-field');
+            linkDiv.classList.add('link-field');
+
+            var input = document.createElement('input');
+            input.id = 'id_link' + (++nextLink);
+            input.type = 'text';
+            input.name = 'link' + nextLink;
+            input.defaultValue = "";
+            input.maxLength = 100;
+            linkDiv.appendChild(input);
+            addDeleteButton(linkDiv);
+            document.querySelector('#link-form').appendChild(linkDiv);
+        },
+        false);
+
+var removeContainer = function(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode)
+}
+
+var addDeleteButton = function(field) {
+    var delButton = document.createElement('a');
+    delButton.addEventListener('click', function() { removeContainer(delButton) }, false);
+    delButton.textContent = '[x]';
+    field.appendChild(delButton)
+};
+
+[].forEach.call(removableInputs, addDeleteButton);
