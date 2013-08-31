f9d23587045389441c46ef85ece4817385c536a8
Mihai Bivol <mm.bivol@gmail.com>
2013-08-31 15:30:14 +0300

    Add login page #49

--- rppl/people/urls.py
+++ rppl/people/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
 
 from views import Overview, Profile, Projects, ProjectDetail, ProfileSetup
 
 urlpatterns = patterns('',
     url(r'^$', Overview.as_view(), name='overview'),
+    url(r'login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'people/login.html'}),
     url(r'^profile/(?P<pk>\d+)/$', Profile.as_view(), name='profile'),
     url(r'^project/$', Projects.as_view(), name='project-list'),
     url(r'^project/(?P<pk>\d+)/$', ProjectDetail.as_view(), name='project-detail'),
