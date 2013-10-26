6167e85b8944f78e087d90540a362bae3ceefa17
Mihai Bivol <mm.bivol@gmail.com>
2013-10-26 21:45:53 +0300

    Quick add of 404 and 500 templates

--- rppl/settings.py
+++ rppl/settings.py
@@ -1,4 +1,4 @@
-# Django settings for rppl project.
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -123,6 +123,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    BASEDIR + '/people/templates/',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
