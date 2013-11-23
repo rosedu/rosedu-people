52a92fe59821db6b80769b685268620dfd274207
Ally <ally@ally.(none)>
2013-11-23 14:19:53 +0200

    Alphabetical order #88

--- rppl/resources/js/grid-search.js
+++ rppl/resources/js/grid-search.js
@@ -49,7 +49,7 @@ var highlightName = function () {
             }
         });
 
-alphabet.sort();
+alphabet.sort(function sortCompare(a,b){return a.localeCompare(b);});
 
 alphabet.forEach(function(e) {
 	var li = document.createElement('li');
